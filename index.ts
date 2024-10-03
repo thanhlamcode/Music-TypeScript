f7af513c3f4cabfb021e9c2144f58a9e6f6f47b5
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-03 22:00:02 +0700

    Kết nối database

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
 import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+
+// Dotenv
+dotenv.config();
+// Dotenv
+
+// Database connect
+database.connect();
+// End Database connect
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
+
+// Pug
+app.set("views", "./views");
+app.set("view engine", "pug");
+// End Pug
 
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("Chủ đề bài hát");
+  res.render("client/pages/topics/index");
 });
 
 app.listen(port, () => {
